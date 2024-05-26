e50def0c94af18863f8f30459122433e033126a5
Temiralp <rtemir1881@gmail.com>
2024-05-26 18:30:19 +0300

    v5

--- test.cs
+++ test.cs
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 test
-    herþey için saðolun hocam
-=======
-//test
-
-Console.WriteLine("SEZER");
->>>>>>> 1d5f65b7ddc565c9166b3241882c988d8be22f7c
-=======
-
-test
-test 222
+ herþey için saðolun hocam
 Console.WriteLine("SEZER");
 
->>>>>>> 755bad5a279d7a5eed4c3fbb30ee43aac9207a59
